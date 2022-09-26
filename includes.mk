8eef9132d271f507ff2167d7f4fc9cfc98352bec
Huszár Bálint <hbalint2100@gmail.com>
2022-09-26 17:08:25 +0200

    hbalint2100 rebase

--- includes.mk
+++ includes.mk
@@ -1,4 +1,4 @@
-SDK = /opt/simplicitystudio/developer/sdks/gecko_sdk_suite/v2.7
+SDK = /mnt/c/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v2.7
 
 INCLUDEFLAGS = -I $(SDK)/hardware/kit/common/bsp
 INCLUDEFLAGS += -I $(SDK)/hardware/kit/common/drivers
@@ -6,4 +6,3 @@ INCLUDEFLAGS += -I $(SDK)/hardware/kit/EFM32GG_STK3700/config
 INCLUDEFLAGS += -I $(SDK)/platform/CMSIS/Include
 INCLUDEFLAGS += -I $(SDK)/platform/Device/SiliconLabs/EFM32GG/Include
 INCLUDEFLAGS += -I $(SDK)/platform/emlib/inc
-
